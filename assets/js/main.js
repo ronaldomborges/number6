56343794a23a90aa5fd95e0fb426bf2ae34c00b3
ronaldomborges <65875791+ronaldomborges@users.noreply.github.com>
2021-04-14 18:58:53 -0300

    544

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@ form.addEventListener('submit', function (e) {
     const imc = getImc(peso, altura)
     const nivelImc = getNivelImc(imc)
 
-    const msg = `<p> Seu IMC é ${imc} (${nivelImc})</p>`
+    const msg = `<p> Seu IMC é ${imc}<br> (${nivelImc})</p>`
     
     setResultado(msg, true)
 })
